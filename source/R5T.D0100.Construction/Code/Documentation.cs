d5d30449d10363c6147e935112ca7deec7673127
davidcoats <david@getsafetycone.com>
2021-11-28 23:08:05 -0800

    Initial commit of source-stub.

---
+++ source/R5T.D0100.Construction/Code/Documentation.cs
@@ -0,0 +1,12 @@
+using System;
+
+
+namespace R5T.D0100.Construction
+{
+    /// <summary>
+    /// Construction project for IFileBasedRepository service..
+    /// </summary>
+    public static class Documentation
+    {
+    }
+}
